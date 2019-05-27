ac56d2368826877fc34f6fc23e6b9da425af8fca
han <is.hanyang@foxmail.com>
2019-05-27 14:38:19 +0800

    路由

--- h5-move-car/src/App.vue
+++ h5-move-car/src/App.vue
@@ -1,41 +1,27 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <!-- <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> -->
-      <img src="./assets/banner.png">
-
-    </div>
-    <div class="content">
-        <div>11111</div>
-        <h4>4444</h4>
-    </div>
-    <router-link to="/Address">输入地址</router-link>
     <router-view/>
-    <!-- <div class="aaa" @click="gotoBuycode">输入地址</div> -->
-    <!-- <Address /> -->
-    <!-- <router-view/> -->
   </div>
 </template>
 <script>
-  import Address from './views/Address.vue'
-  export default {
-    name: 'app',
-    components: {
-      Address
-    },
-    methods:{
+  // import Address from './views/Address.vue'
+  // export default {
+  //   name: 'app',
+  //   components: {
+  //     Address
+  //   },
+  //   methods:{
 
-      // gotoBuycode() {
-      //   console.log("ddd");
-      //   this.$router.push({
-      //     path: "/Address",
+  //     // gotoBuycode() {
+  //     //   console.log("ddd");
+  //     //   this.$router.push({
+  //     //     path: "/Address",
           
-      //   })
+  //     //   })
         
-      // }
-    }
-  }
+  //     // }
+  //   }
+  // }
 </script>
 
 <style>
@@ -45,33 +31,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-#nav {
-  width: 100%;
-  
-}
-
-#nav img{
-  width: 100%;
-  vertical-align: middle;
-}
-
-/* #nav a.router-link-exact-active {
-  color: #42b983;
-} */
-.content{
-  width: 90%;
-  height: 10rem;
-  background: #f0f;
-  margin: 0 auto;
-  font-size: 0.3rem;
-  position: relative;
-  top: -2rem;
-}
-.aaa {
-  font-size: 0.5rem;
-  width: 50%;
-  margin: 0 auto;
-  background: #ff0;
 }
 </style>
--- h5-move-car/src/views/Home.vue
+++ h5-move-car/src/views/Home.vue
@@ -1,18 +1,63 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div id="nav">
+        <!-- <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link> -->
+        <img src="./../assets/banner.png">
+
+      </div>
+      <div class="content">
+          <div>11111</div>
+          <h4>4444</h4>
+          <router-link to="/Address">输入地址</router-link>
+      </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  export default {
+    name: 'app',
+    methods:{
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
+      // gotoBuycode() {
+      //   console.log("ddd");
+      //   this.$router.push({
+      //     path: "/Address",
+          
+      //   })
+        
+      // }
+    }
   }
-}
 </script>
+<style scoped>
+#nav {
+  width: 100%;
+  
+}
+
+#nav img{
+  width: 100%;
+  vertical-align: middle;
+}
+
+/* #nav a.router-link-exact-active {
+  color: #42b983;
+} */
+.content{
+  width: 90%;
+  height: 10rem;
+  background: #f0f;
+  margin: 0 auto;
+  font-size: 0.3rem;
+  position: relative;
+  top: -2rem;
+}
+.aaa {
+  font-size: 0.5rem;
+  width: 50%;
+  margin: 0 auto;
+  background: #ff0;
+}
+</style>
+
